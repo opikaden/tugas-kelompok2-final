e53eff809c2d33c792bbaf26407b780dcaf3aeb6
opikaden <taufiqqurrahman102@gmail.com>
2023-11-14 22:40:45 +0700

    Web kelompok 2 final

---
+++ script.js
@@ -0,0 +1,29 @@
+// Nav Menu jika saat aktif
+const navbarNav = document.querySelector(".navbar-nav");
+// Saat Menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Searcg jika aktif
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar elemen untuk menutup elemen
+const sidebar = document.querySelector("#menu");
+const search = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!sidebar.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
